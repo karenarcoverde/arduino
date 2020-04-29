391ea75f134d89c9c7b507a7b2f762b1f716eae5
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2020-04-29 17:00:49 -0300

    Add files via upload

---
+++ lm35/lm35.ino
@@ -0,0 +1,33 @@
+#define pino A0
+
+
+int temperatura = 0;
+int ultima_temperatura = 0;
+int media[10];
+
+void setup() 
+{
+ Serial.begin (9600);
+}
+
+void loop() 
+{
+ 
+  for (int indice = 0; indice < 10; indice++)
+  {
+    media[indice] = (analogRead (pino)*(5.0/1023))/0.01;
+    temperatura = temperatura + media[indice];
+    delay(100);
+  }
+  
+  temperatura = temperatura/10.0;
+
+ if(temperatura != ultima_temperatura)
+ {
+  ultima_temperatura = temperatura;
+  Serial.print ("Temperatura: ");
+  Serial.print (temperatura);
+  Serial.println ("°C");
+ }
+ delay(1000);
+}
